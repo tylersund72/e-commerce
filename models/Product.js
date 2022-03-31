2cf5b3b5c6a4d3440ca6c5638d1152f1331f9595
Tyler <tylersundquist3@gmail.com>
2022-03-31 15:34:01 -0600

    push for askBCS

--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,6 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const { Category } = require(".");
 const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
@@ -38,7 +37,7 @@ Product.init(
     category_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: Category,
+        model: "Category",
         key: "id",
       },
     },
