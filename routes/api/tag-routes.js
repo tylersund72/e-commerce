1c2c952316c2d51aa391b4d3226df7c496227cd4
Tyler <tylersundquist3@gmail.com>
2022-03-31 16:17:17 -0600

    slight code refactor

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -29,7 +29,7 @@ router.get("/:id", (req, res) => {
     include: [
       {
         model: Product,
-        attributes: ["product_name", "price,", "stock", "category_id"],
+        attributes: ["id", "product_name", "price,", "stock", "category_id"],
       },
     ],
   })
